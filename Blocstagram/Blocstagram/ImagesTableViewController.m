b6ab8446f6c2ddd8968ccdfdb25780de5d851954
abnero <abnero@gmail.com>
2015-10-03 21:02:25 -0700

    changes to checkpoint 27

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -22,8 +22,8 @@
         UIImage *image = [UIImage imageNamed:imageName];
         if (image) {
             [self.images addObject:image];
-        } else if (!image){
-            [self.images delete:image];
+//        } else if (!image){
+//            [self.images delete:image];
         }
     }
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
@@ -96,22 +96,22 @@
 }
 
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
 
-// Override to support editing the table view.
+
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        [tableView reloadData]; // tell table to refresh now
+       
     }   
 }
 
