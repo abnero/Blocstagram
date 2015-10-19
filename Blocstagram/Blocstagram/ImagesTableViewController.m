f798ff9b2c18d546c723b16c1010fd63de356a11
abnero <abnero@gmail.com>
2015-10-19 11:46:39 -0700

    checkpoint 41 completed

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -15,8 +15,9 @@
 #import "Comment.h"
 #import "MediaTableViewCell.h"
 #import "MediaFullScreenViewController.h"
+#import "CameraViewController.h"
 
-@interface ImagesTableViewController () <MediaTableViewCellDelegate, UIViewControllerTransitioningDelegate> //UIViewControllerTransitioningDelegate is for the assignment of checkpoint 36 View and Sharing
+@interface ImagesTableViewController () <MediaTableViewCellDelegate, UIViewControllerTransitioningDelegate, CameraViewControllerDelegate> //UIViewControllerTransitioningDelegate is for the assignment of checkpoint 36 View and Sharing
 
 
 //back button
@@ -50,6 +51,13 @@
     
     
     self.tableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] ||
+        [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum]) {
+        UIBarButtonItem *cameraButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(cameraPressed:)];
+        self.navigationItem.rightBarButtonItem = cameraButton;
+    }
+    
     
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(keyboardWillShow:)
@@ -130,6 +138,27 @@
 - (void) viewWillDisappear:(BOOL)animated {
     
 }
+
+#pragma mark - Camera and CameraViewControllerDelegate
+
+- (void) cameraPressed:(UIBarButtonItem *) sender {
+    CameraViewController *cameraVC = [[CameraViewController alloc] init];
+    cameraVC.delegate = self;
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:cameraVC];
+    [self presentViewController:nav animated:YES completion:nil];
+    return;
+}
+
+- (void) cameraViewController:(CameraViewController *)cameraViewController didCompleteWithImage:(UIImage *)image {
+    [cameraViewController dismissViewControllerAnimated:YES completion:^{
+        if (image) {
+            NSLog(@"Got an image!");
+        } else {
+            NSLog(@"Closed without an image.");
+        }
+    }];
+}
+
 
 
 
---
+++ Blocstagram/Blocstagram/UIImage+ImageUtilities.h
@@ -0,0 +1,21 @@
+//
+//  UIImage+ImageUtilities.h
+//  Blocstagram
+//
+//  Created by Abner Flores on 10/19/15.
+//  Copyright (c) 2015 Abner Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ImageUtilities)
+
+- (UIImage *) imageWithFixedOrientation;
+- (UIImage *) imageResizedToMatchAspectRatioOfSize:(CGSize)size;
+- (UIImage *) imageCroppedToRect:(CGRect)cropRect;
+
+@end
+
+@interface UIImage (ImageUtilities)
+
+@end
