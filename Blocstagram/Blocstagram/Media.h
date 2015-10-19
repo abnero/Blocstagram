de4c909d632a670caf1e182079c8619a4d17e155
abnero <abnero@gmail.com>
2015-10-18 20:57:13 -0700

    checkpont 40 completed

--- Blocstagram/Blocstagram/Media.h
+++ Blocstagram/Blocstagram/Media.h
@@ -30,6 +30,9 @@ typedef NS_ENUM(NSInteger, MediaDownloadState) {
 @property (nonatomic, strong) NSArray *comments;
 @property (nonatomic, assign) LikeState likeState;
 
+@property (nonatomic, strong) NSString *temporaryComment;
+
+
 
 @property (nonatomic, assign) MediaDownloadState downloadState;
 
