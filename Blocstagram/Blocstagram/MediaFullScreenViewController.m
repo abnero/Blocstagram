f58685847ed0e338cb5c99112a6073227d2e410a
abnero <abnero@gmail.com>
2015-10-21 17:47:07 -0700

    checkpoint 42 in progress

--- Blocstagram/Blocstagram/MediaFullScreenViewController.m
+++ Blocstagram/Blocstagram/MediaFullScreenViewController.m
@@ -14,7 +14,7 @@
 
 @interface MediaFullScreenViewController () <UIScrollViewDelegate>
 
-@property (nonatomic, strong) Media *media;
+
 @property (nonatomic, strong) UITapGestureRecognizer *tap;
 @property (nonatomic, strong) UITapGestureRecognizer *doubleTap;
 @property (nonatomic, strong) UIWebView *webview;
@@ -102,9 +102,17 @@
     // #4
     self.scrollView.frame = self.view.bounds;
     
-    // #5
+    [self recalculateZoomScale];
+}
+
+- (void) recalculateZoomScale {
+    
     CGSize scrollViewFrameSize = self.scrollView.frame.size;
     CGSize scrollViewContentSize = self.scrollView.contentSize;
+    
+    scrollViewContentSize.height /= self.scrollView.zoomScale;
+    scrollViewContentSize.width /= self.scrollView.zoomScale;
+    
     
     CGFloat scaleWidth = scrollViewFrameSize.width / scrollViewContentSize.width;
     CGFloat scaleHeight = scrollViewFrameSize.height / scrollViewContentSize.height;
